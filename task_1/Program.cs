3772e9114e9c39e3b1fffe6ec85394945a24f2e8
Denis.Torgov <deniska-3000@bk.ru>
2022-12-08 19:18:21 +0300

    task 1 stage Fill array

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string GetNewString()
+{
+    string a = Convert.ToString(Console.ReadLine());
+    return a;
+}
+
+string[] FillStringArray()
+{
+    string[] arr = new string [0];
+    string b = "a";
+    for (int i = 0; (b = GetNewString()) != " "; i++)
+    {
+        Array.Resize(ref arr, i);
+        arr[i] = b;
+    }
+    return arr;
+}
+
+string a = FillStringArray;
+
+PrintArray(a);
+FilterArray(a);
+PrintArray(a);
+
